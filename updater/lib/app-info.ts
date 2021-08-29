a5dd9e6a8bc7479562b6b19ca7d497cd58bbc4fe
Jakob Norlin <jakob.stahl91@gmail.com>
2021-08-29 22:12:37 +0200

    Release v0.2.0

--- updater/lib/app-info.ts
+++ updater/lib/app-info.ts
@@ -2,7 +2,7 @@ export const appInfo = {
   name: 'MM Lunch',
   description: 'Remember your lunch expenses',
   latest: {
-    version: '0.1.0',
-    binary: '/releases/mm-lunch-0.1.0.app.tar.gz',
+    version: '0.2.0',
+    binary: '/releases/mm-lunch-0.2.0.app.tar.gz',
   },
 }
